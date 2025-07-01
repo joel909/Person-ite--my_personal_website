d9cf832ae38326b211a1a04beeca7cca27e53913
Joel Joby <joeljobyp@gmail.com>
2025-07-01 18:10:29 +0530

    completed website

--- api/index.py
+++ api/index.py
@@ -8,6 +8,15 @@ def home():
 @app.route('/favicon.ico')
 def favicon():
     return url_for('static', filename='favicon.ico')
+@app.route('/404', methods=['GET'])
+def not_found():
+    return render_template('404.html')
+
+@app.route('/insta', methods=['GET'])
+def insta():
+    return render_template('insta.html')
 
 while True:
     app.run(debug=True,host="0.0.0.0",port=5000)
+
+##
